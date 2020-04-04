e3b9297d0d0b0a9dd02242eef8a475200eee2248
Janosch <janosch.baltensperger@gmail.com>
2020-04-04 16:57:56 +0200

    extending player creation 2

--- src/main/java/ch/uzh/ifi/seal/soprafs20/controller/GameController.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/controller/GameController.java
@@ -3,8 +3,6 @@ package ch.uzh.ifi.seal.soprafs20.controller;
 import ch.uzh.ifi.seal.soprafs20.entity.*;
 import ch.uzh.ifi.seal.soprafs20.rest.dto.game.GameGetDTO;
 import ch.uzh.ifi.seal.soprafs20.rest.dto.game.GamePostDTO;
-import ch.uzh.ifi.seal.soprafs20.rest.dto.user.UserGetDTO;
-import ch.uzh.ifi.seal.soprafs20.rest.mapper.DTOMapper;
 import ch.uzh.ifi.seal.soprafs20.rest.mapper.GameDTOMapper;
 import ch.uzh.ifi.seal.soprafs20.service.GameService;
 import org.springframework.http.HttpStatus;
