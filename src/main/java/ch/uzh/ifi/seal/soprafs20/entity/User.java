09ee5fbcd4999dd41ae080c0910062719204acf2
Janosch <janosch.baltensperger@gmail.com>
2020-04-05 16:10:33 +0200

    fixing autoincrement issue

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity/User.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue
+    @GeneratedValue
 	private Long id;
 	
 	@Column(nullable = false)
