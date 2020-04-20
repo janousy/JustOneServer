b3d2bbdcc263aa5bf134267323d36f22666399b0
Janosch <janosch.baltensperger@gmail.com>
2020-04-20 15:22:42 +0200

    working lemmatization

---
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/controller/RoundControllerTest.java
@@ -0,0 +1,7 @@
+package ch.uzh.ifi.seal.soprafs20.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(PlayerController.class)
+public class RoundControllerTest {
+}
