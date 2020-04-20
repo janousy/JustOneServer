491676a6da140a4f362b0b3d84ec3435f7c14a4a
Janosch <janosch.baltensperger@gmail.com>
2020-04-20 11:25:26 +0200

    working lemmatization

--- src/main/java/ch/uzh/ifi/seal/soprafs20/service/HintValidationService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/service/HintValidationService.java
@@ -211,33 +211,4 @@ public class HintValidationService {
         }
         return copyHints;
     }
-
-    private static final String GET_URL = "https://localhost:9090/SpringMVCExample";
-
-    public void sendGET() throws IOException {
-        log.debug("sent GET");
-        URL obj = new URL(GET_URL);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", USER_AGENT);
-        int responseCode = con.getResponseCode();
-        System.out.println("GET Response Code :: " + responseCode);
-        if (responseCode == HttpURLConnection.HTTP_OK) { // success
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // print result
-            System.out.println(response.toString());
-        }
-        else {
-            System.out.println("GET request not worked");
-        }
-    }
 }
